fd04fa407045022c170c09d28e24c71c63e31d31
Ahmed Ehab <ahmed.ehab.h@gmail.com>
2020-08-17 01:31:08 +0200

    remove mongodb
    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask_mongoalchemy import MongoAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
@@ -8,9 +7,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b61193399df06673c88d402db7a45759'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# app.config['MONGOALCHEMY_DATABASE'] = 'mongodb+srv://ahmedehab:Password@quiz-hvhc1.gcp.mongodb.net/GP?retryWrites=true&w=majority'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# db = MongoAlchemy(app)
 bcrypt = Bcrypt()
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
